31401f6032b31fba77cabebc9221ae96f96dffc3
Washington Sousa <washington.inf@gmail.com>
2020-04-04 20:54:49 -0300

    Startup sofisticado.

--- Backend/src/Core/Application/Application/Providers/OAuthProvider.cs
+++ Backend/src/Core/Application/Application/Providers/OAuthProvider.cs
@@ -6,6 +6,7 @@ using Core.Data.Models;
 using Core.Shared.Kernel.Events;
 using Application;
 using Core.Application.Aggregates;
+using System.Linq;
 
 namespace Core.App.Providers
 {
@@ -13,19 +14,19 @@ namespace Core.App.Providers
     {
     
         private Claim AccessLevelClaim;
-        private Usuario usuario;
+        private Usuario Usuario;
 
         public OAuthProvider() {}
 
         public override Task TokenEndpoint(OAuthTokenEndpointContext context)
         {
-            context.AdditionalResponseParameters.Add("access_level", this.AccessLevelClaim.Value);
+            context.AdditionalResponseParameters.Add("access_level", AccessLevelClaim?.Value);
 
-            if(this.usuario != null)
+            if(this.Usuario != null)
               {
-                context.AdditionalResponseParameters.Add("user_id", this.usuario.Id);
-                context.AdditionalResponseParameters.Add("username", this.usuario.Nome);
-                context.AdditionalResponseParameters.Add("email", this.usuario.Email);
+                context.AdditionalResponseParameters.Add("user_id", Usuario.Id);
+                context.AdditionalResponseParameters.Add("username", Usuario.Nome);
+                context.AdditionalResponseParameters.Add("email", Usuario.Email);
               }
             
      
@@ -42,6 +43,8 @@ namespace Core.App.Providers
                 string password  = context.Password;
 
                 UsuarioAuthenticationTicketAggregate aggr = _service.Authenticate(userName, password);
+                Usuario = aggr.Usuario;
+                AccessLevelClaim = aggr.Ticket.Identity.Claims.Where(c => c.Type == ClaimTypes.Role).FirstOrDefault();
 
                 if (!aggr.Valid)
                 {
